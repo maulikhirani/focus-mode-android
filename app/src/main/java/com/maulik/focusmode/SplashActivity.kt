5be5c84cccdf3ca51e6ecfd4e2416d0b9b8658ce
Maulik Hirani <maulikhirani@gmail.com>
2020-06-01 09:08:45 +0530

    Initial Project Setup with some prototype

---
+++ app/src/main/java/com/maulik/focusmode/SplashActivity.kt
@@ -0,0 +1,44 @@
+package com.maulik.focusmode
+
+import android.animation.Animator
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.maulik.focusmode.databinding.LayoutSplashBinding
+import com.maulik.focusmode.extensions.playAnimation
+import com.maulik.focusmode.extensions.startActivityAndFinish
+import render.animations.Attention
+import render.animations.Bounce
+import render.animations.Render
+
+class SplashActivity : AppCompatActivity() {
+
+    lateinit var binding: LayoutSplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.layout_splash)
+
+        binding.logoAnim.addAnimatorListener(object : Animator.AnimatorListener {
+
+            override fun onAnimationRepeat(animation: Animator?) {
+            }
+
+            override fun onAnimationEnd(animation: Animator?) {
+                startActivityAndFinish<MainActivity>()
+            }
+
+            override fun onAnimationCancel(animation: Animator?) {
+            }
+
+            override fun onAnimationStart(animation: Animator?) {
+            }
+
+        })
+
+        val renderer = Render(this)
+        renderer.setAnimation(Bounce().In(binding.appTitle))
+        renderer.start()
+
+    }
+}
---
+++ app/src/main/java/com/maulik/focusmode/extensions/CustomExtensions.kt
@@ -0,0 +1,43 @@
+package com.maulik.focusmode.extensions
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import androidx.annotation.AnimRes
+
+inline fun <reified T : Activity> Context.startActivity(block: Intent.() -> Unit = {}) {
+    val intent = Intent(this, T::class.java)
+    block(intent)
+    startActivity(intent)
+}
+
+inline fun <reified T : Activity> Activity.startActivityAndFinish(block: Intent.() -> Unit = {}) {
+    val intent = Intent(this, T::class.java)
+    block(intent)
+    startActivity(intent)
+    finish()
+}
+
+private class AnimationListener(
+    private val onAnimationRepeat: () -> Unit,
+    private val onAnimationStart: () -> Unit,
+    private val onAnimationEnd: () -> Unit
+) : Animation.AnimationListener {
+    override fun onAnimationRepeat(p0: Animation?) = onAnimationRepeat()
+    override fun onAnimationStart(p0: Animation?) = onAnimationStart()
+    override fun onAnimationEnd(p0: Animation?) = onAnimationEnd()
+}
+
+fun View.playAnimation(
+    @AnimRes animResId: Int,
+    onAnimationRepeat: () -> Unit = {},
+    onAnimationStart: () -> Unit = {},
+    onAnimationEnd: () -> Unit = {}
+) = with(AnimationUtils.loadAnimation(context, animResId)) {
+    setAnimationListener(AnimationListener(onAnimationRepeat, onAnimationStart, onAnimationEnd))
+    startAnimation(this)
+}
